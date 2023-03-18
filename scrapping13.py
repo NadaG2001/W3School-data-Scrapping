a57740717725289dcad072fd46ac284046b8b037
Nada Gamal Mhmd <110260350+NadaG2001@users.noreply.github.com>
2023-03-18 11:12:51 +0200

    Add files via upload

---
+++ scrapping13.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Send a GET request to the C++ tutorials page
+url = "https://www.w3schools.com/cpp/default.asp"
+response = requests.get(url)
+
+# Parse the HTML content using Beautiful Soup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the tutorial titles and links on the page
+tutorials = soup.select("#leftmenuinnerinner a")
+
+# Write the tutorial titles and links to a CSV file
+with open("w3schools_cpp_tutorials.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Link"])
+    for tutorial in tutorials:
+        title = tutorial.text.strip()
+        link = "https://www.w3schools.com" + tutorial["href"]
+        writer.writerow([title, link])
